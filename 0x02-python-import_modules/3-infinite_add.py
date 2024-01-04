db873efaa3164f873a594b301f56c92ff9e555aa
tasawo9 <el892238@gmail.com>
2024-01-04 07:56:21 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,4 @@ if __name__ == "__main__":
     sum = 0
     for a in range(1, len(argv)):
         sum += int(argv[a])
-    print(sum#!/usr/bin/python3
-            if __name__ == "__main__":
-                from sys import argv
-                    sum = 0
-                        for a in range(1, len(argv)):
-                                sum += int(argv[a])
-                                    print(sum))
+    print(sum)
