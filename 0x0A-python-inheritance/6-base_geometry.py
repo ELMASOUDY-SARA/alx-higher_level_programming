694c0201e37463ae8292181a1d2fb12c3195fb32
tasawo9 <el892238@gmail.com>
2024-02-05 15:25:26 -0800

    s

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Contains the class BaseGeometry
-"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-        """A class with public attribute area"""
-            def area(self):
-                        """raises an exception when called"""
-                                raise Exception("area() is not implemented")
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented yet")
